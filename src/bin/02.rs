5d5ee1086cefb1a0c57f2962588695c7d8b5a195
Seth Gower <sethzerish@gmail.com>
2022-12-03 08:50:27 -0500

    :poop: updates day 2 with solution to part 2. need to rework

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,5 +1,6 @@
 use std::cmp::Ord;
 use std::cmp::Ordering;
+use std::ops::Neg;
 #[derive(Eq, PartialEq, Copy, Clone, Debug, Ord)]
 enum Choice {
     Rock = 1,
@@ -29,17 +30,16 @@ impl PartialOrd for Choice {
     }
 }
 
-
 #[derive(Eq, PartialEq, Copy, Clone, Debug)]
-struct Game {
+struct Game_Part1 {
     left: Option<Choice>,
     right: Option<Choice>,
     left_score: u32,
     right_score: u32,
 }
-impl Game {
-    fn new(left: &str, right: &str) -> Option<Game> {
-        let mut rv: Game = Game {
+impl Game_Part1 {
+    fn new(left: &str, right: &str) -> Option<Game_Part1> {
+        let mut rv: Game_Part1 = Game_Part1 {
             left: None,
             right: None,
             left_score: 0,
@@ -72,14 +72,107 @@ impl Game {
         Some(rv)
     }
 }
+#[derive(Eq, PartialEq, Copy, Clone, Debug)]
+struct Player {
+    choice: Option<Choice>,
+    outcome: Option<Outcome>,
+}
 
+#[derive(Eq, PartialEq, Copy, Clone, Debug)]
+struct Game_Part2 {
+    left: Option<Player>,
+    right: Option<Player>,
+}
+
+#[derive(Eq, PartialEq, Copy, Clone, Debug)]
+enum Outcome {
+    Loss = 0,
+    Draw = 3,
+    Win = 6,
+}
+impl Neg for Outcome {
+    type Output = Self;
+    fn neg(self) -> Self::Output {
+        match self {
+            Outcome::Loss => Outcome::Win,
+            Outcome::Draw => Outcome::Draw,
+            Outcome::Win => Outcome::Loss,
+        }
+    }
+}
+impl Game_Part2 {
+    fn new(left: &str, right: &str) -> Option<Game_Part2> {
+        let mut rv: Game_Part2 = Game_Part2 {
+            left: None,
+            right: None,
+        };
+        rv.right = Some(Player {
+            choice: None,
+            outcome: match right {
+                "X" => Some(Outcome::Loss),
+                "Y" => Some(Outcome::Draw),
+                "Z" => Some(Outcome::Win),
+                _ => None,
+            },
+        });
+        rv.left = match left {
+            "A" => Some(Player {
+                choice: Some(Choice::Rock),
+                outcome: Some(-rv.right?.outcome?),
+            }),
+            "B" => Some(Player {
+                choice: Some(Choice::Paper),
+                outcome: Some(-rv.right?.outcome?),
+            }),
+            "C" => Some(Player {
+                choice: Some(Choice::Scissors),
+                outcome: Some(-rv.right?.outcome?),
+            }),
+            _ => None,
+        };
+        if let Some(mut player) = rv.right {
+            player.choice = Game_Part2::decide_choice(-player.outcome?, rv.left?.choice?);
+            rv.right = Some(player);
+        }
+        Some(rv)
+    }
+    fn decide_choice(outcome: Outcome, other_player: Choice) -> Option<Choice> {
+        if outcome == Outcome::Draw {
+            return Some(other_player);
+        } else {
+            match other_player {
+                Choice::Rock => {
+                    if outcome == Outcome::Win {
+                        Some(Choice::Scissors)
+                    } else {
+                        Some(Choice::Paper)
+                    }
+                }
+                Choice::Paper => {
+                    if outcome == Outcome::Win {
+                        Some(Choice::Rock)
+                    } else {
+                        Some(Choice::Scissors)
+                    }
+                }
+                Choice::Scissors => {
+                    if outcome == Outcome::Win {
+                        Some(Choice::Paper)
+                    } else {
+                        Some(Choice::Rock)
+                    }
+                }
+            }
+        }
+    }
+}
 pub fn part_one(input: &str) -> Option<u32> {
     let mut sum = 0;
     let lines: Vec<&str> = input.lines().collect();
 
     for line in lines {
         let mut line = line.split(' ');
-        let game = Game::new(line.next()?, line.next()?)?;
+        let game = Game_Part1::new(line.next()?, line.next()?)?;
         sum += game.right_score;
     }
 
@@ -87,7 +180,18 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let lines: Vec<&str> = input.lines().collect();
+    let mut sum: u32 = 0;
+
+    for line in lines {
+        let mut line = line.split(' ');
+        let game = Game_Part2::new(line.next()?, line.next()?)?;
+        if let Some(right) = game.right {
+            sum += right.outcome? as u32;
+            sum += right.choice? as u32;
+        }
+    }
+    Some(sum)
 }
 
 fn main() {
