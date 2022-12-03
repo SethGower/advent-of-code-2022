cd268bb5f900bb9a5089cc6d49c541ea96c2e844
Seth Gower <sethzerish@gmail.com>
2022-12-03 10:52:00 -0500

    day03: Submits solution for day 3 part 2

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -28,8 +28,48 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(sum)
 }
 
+fn intersection(mut sets: Vec<HashSet<char>>) -> HashSet<char> {
+    if sets.is_empty() {
+        return HashSet::new();
+    }
+
+    if sets.len() == 1 {
+        return sets.pop().unwrap();
+    }
+
+    let mut result = sets.pop().unwrap();
+    result.retain(|item| sets.iter().all(|set| set.contains(item)));
+    result
+}
+
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let lines: Vec<&str> = input.lines().collect();
+    let mut lines_iter = lines.iter();
+    let mut priorities = vec![];
+    for i in 0..lines.len() / 3 {
+        let mut sets: Vec<HashSet<char>> = vec![];
+        for _ in 0..3 {
+            let mut set = HashSet::new();
+            let line = lines_iter.next()?;
+            for char in line.chars() {
+                set.insert(char);
+            }
+            sets.push(set);
+        }
+        let int = intersection(sets);
+        priorities.push(int);
+    }
+    let rv = priorities.iter().map(|h| -> u32 {
+        let ch = *h.iter().next().unwrap();
+        let rv = match ch as u32 {
+            65..=90 => ch as u32 - 38,
+            97..=122 => ch as u32 - 96,
+            _ => 0,
+        };
+        rv
+
+    }).sum();
+    Some(rv)
 }
 
 fn main() {
@@ -51,6 +91,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 3);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(70));
     }
 }
