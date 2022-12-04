125ce42cf24aaae61e6362fd2d0d746bd8cca1c1
Seth Gower <sethzerish@gmail.com>
2022-12-04 10:46:44 -0500

    day04: adds part 2

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -27,7 +27,34 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let overlaps = input
+        .lines()
+        .filter(|line| {
+            let mut sets: Vec<HashSet<u32>> = vec![];
+            for elf in line.split(',') {
+                let mut elf = elf.split('-');
+                let left = elf.next().unwrap().parse::<u32>().unwrap();
+                let right = elf.next().unwrap().parse::<u32>().unwrap();
+                let mut set: HashSet<u32> = HashSet::new();
+                for j in left..=right {
+                    set.insert(j);
+                }
+                sets.push(set);
+            }
+            if sets
+                .get(0)
+                .unwrap()
+                .intersection(sets.get(1).unwrap())
+                .count()
+                > 0
+            {
+                true
+            } else {
+                false
+            }
+        })
+        .count();
+    Some(overlaps as u32)
 }
 
 fn main() {
@@ -43,12 +70,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 4);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(2));
     }
 
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 4);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(4));
     }
 }
