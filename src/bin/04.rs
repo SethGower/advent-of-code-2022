c2ecee54027d326c1875bd37783a656044f674c3
Seth Gower <sethzerish@gmail.com>
2022-12-04 15:03:31 -0500

    day04: reworks part 1 without sets, uses regex. is about 4x faster

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,26 +1,29 @@
 use std::collections::HashSet;
+use regex::Regex;
+#[derive(Debug)]
+struct Elf {
+    left: u32,
+    right: u32,
+}
+impl Elf {
+    fn subset(&self, other: &Self) -> bool {
+        self.left >= other.left && self.right <= other.right
+    }
+}
 pub fn part_one(input: &str) -> Option<u32> {
-    let overlaps = input
-        .lines()
-        .filter(|line| {
-            let mut sets: Vec<HashSet<u32>> = vec![];
-            for elf in line.split(',') {
-                let mut elf = elf.split('-');
-                let left = elf.next().unwrap().parse::<u32>().unwrap();
-                let right = elf.next().unwrap().parse::<u32>().unwrap();
-                let mut set: HashSet<u32> = HashSet::new();
-                for j in left..=right {
-                    set.insert(j);
-                }
-                sets.push(set);
-            }
-            if sets.get(0).unwrap().is_subset(sets.get(1).unwrap())
-                || sets.get(1).unwrap().is_subset(sets.get(0).unwrap())
-            {
-                true
-            } else {
-                false
-            }
+    let re = Regex::new(r"(\d+)-(\d+),\s*(\d+)-(\d+)").ok()?;
+    let overlaps = re
+        .captures_iter(input)
+        .filter(|cap| {
+            let left = Elf {
+                left: cap[1].parse::<u32>().unwrap(),
+                right: cap[2].parse::<u32>().unwrap(),
+            };
+            let right = Elf {
+                left: cap[3].parse::<u32>().unwrap(),
+                right: cap[4].parse::<u32>().unwrap(),
+            };
+            right.subset(&left) || left.subset(&right)
         })
         .count();
     Some(overlaps as u32)
